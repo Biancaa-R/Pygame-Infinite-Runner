cb03f0425c0e9dab366d565ca91027523def8767
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2022-09-19 16:57:58 +0530

    Add files via upload

--- Pygame infinite runner/main.py
+++ Pygame infinite runner/main.py
@@ -54,9 +54,16 @@ def player_animation():
 def coin_animation():
     global coin_surface,coin_index
     coin_index+=0.1
-    if coin_index>len(coin_spin):
+    if coin_index>=len(coin_spin):
         coin_index=0
     coin_surface=coin_spin[int(coin_index)]
+
+def primary_snail_animation():
+    global snail_surface,snail_index
+    snail_index+=0.1
+    if snail_index>=len(snail):
+        snail_index=0
+    snail_surface=snail[int(snail_index)]    
         
 pygame.init()
 screen=pygame.display.set_mode((800,480))#pygame.RESIZABLE)#creating a display surface
@@ -75,12 +82,30 @@ time=0
 score=0
 gravity=0
 game_running=False
+global game_level
+game_level=0
 
 
 #objects
-bg_surface=pygame.Surface((800,300))
-bg_surface=pygame.image.load("graphics/Sky.png")
 
+sky1=pygame.image.load("graphics/Sky.png")
+sky2=pygame.image.load("graphics/Sky1.jpg")
+sky3=pygame.image.load("graphics/Sky2.png")
+sky4=pygame.image.load("graphics/Sky3.jpg")
+sky5=pygame.image.load("graphics/Sky4.jpg")
+sky6=pygame.image.load("graphics/Sky5.png")
+sky7=pygame.image.load("graphics/Sky6.jpg")
+sky8=pygame.image.load("graphics/Sky7.jpg")
+sky9=pygame.image.load("graphics/Sky8.jpg")
+sky10=pygame.image.load("graphics/Sky9.png")
+sky11=pygame.image.load("graphics/Sky11.jpg")
+sky12=pygame.image.load("graphics/Sky12.jpg")
+sky13=pygame.image.load("graphics/Sky13.jpg")
+sky=[sky1,sky2,sky3,sky4,sky5,sky6,sky7,sky8,sky9,sky10,sky11,sky12,sky13]
+if game_level>12:
+    game_level=0
+bg_surface=pygame.Surface((800,300))
+bg_surface=sky[game_level]
 ground_surface=pygame.Surface((800,200))
 ground_surface=pygame.image.load("graphics/ground.png")
 
@@ -101,17 +126,25 @@ coin_rect=coin_surface.get_rect(bottomleft=(300,300))
 
 #characters
 player_index=0
-player_walk_1=pygame.image.load("graphics/player_walk_1.png")
-player_walk_2=pygame.image.load("graphics/player_walk_2.png")
-player_jump=pygame.image.load("graphics/jump.png")
+player_walk_1=pygame.image.load("graphics/player_walk_1.png").convert_alpha()
+player_walk_2=pygame.image.load("graphics/player_walk_2.png").convert_alpha()
+player_jump=pygame.image.load("graphics/jump.png").convert_alpha()
 player_walk=[player_walk_1,player_walk_2]
 player_surface=player_walk[player_index]
 player_rect=player_surface.get_rect(bottomleft=(50,300))
 
-snail_surface=pygame.image.load("graphics/snail1.png")
+snail_index=0
+snail1=pygame.image.load("graphics/snail1.png").convert_alpha()
+snail2=pygame.image.load("graphics/snail2.png").convert_alpha()
+snail=[snail1,snail2]
+snail_surface=snail[snail_index]
 snail_rect=snail_surface.get_rect(midbottom=(770,300))
 
-fly_surface=pygame.image.load("graphics/Fly1.png")
+fly_index=0
+fly1=pygame.image.load("graphics/Fly1.png").convert_alpha()
+fly2=pygame.image.load("graphics/Fly2.png").convert_alpha()
+fly=[fly1,fly2]
+fly_surface=fly[fly_index]
 
 player_stand=pygame.image.load("graphics/player_stand.png")
 player_stand=pygame.transform.scale2x(player_stand) #Transforming the player_surf
@@ -119,6 +152,9 @@ playerstand_rect=player_stand.get_rect(center=(360,260))
 
 obstracle_timer=pygame.USEREVENT+1
 pygame.time.set_timer(obstracle_timer,1800)
+
+snail_animation_timer=pygame.USEREVENT+2
+pygame.time.set_timer(snail_animation_timer,200)
 
 #Game over
 #game_font=pygame.font.Font(None,50)
@@ -142,6 +178,18 @@ while True:
                 if choice in ["fly",]:
                     obstracle_rect_list.append(fly_surface.get_rect(midbottom=(position,200)))
 
+            if event.type==snail_animation_timer:
+                for obstracle in obstracle_rect_list:
+                    if obstracle.bottom==300:
+                        snail_index+=0.3
+                        if snail_index>=len(snail):
+                            snail_index=0
+                        snail_surface=snail[int(snail_index)]
+                    else:
+                        fly_index+=0.3
+                        if fly_index>=len(fly):
+                            fly_index=0
+                        fly_surface=fly[int(fly_index)]
             if event.type==pygame.KEYDOWN and player_rect.colliderect(snail_rect)==False:
                 if event.key==pygame.K_UP and player_rect.bottom>=100:
                     gravity = -15
@@ -156,6 +204,8 @@ while True:
                     game_running=True
                     obstracle_rect_list=[]
                     snail_rect.right=800
+                    if score>=3:
+                        game_level+=1
                     score=0
                     starting_time=pygame.time.get_ticks()//1000
                     
@@ -176,6 +226,12 @@ while True:
         display_time()
         player_animation()
         coin_animation()
+        primary_snail_animation()
+
+        if game_level>12:
+            game_level=0
+        bg_surface=sky[game_level]
+        
         #gravity mechanism:
         gravity+=1
         player_rect.y+=gravity
@@ -231,7 +287,7 @@ while True:
         #To account for the delay in game loading (rarely)
         
         screen.fill("salmon")
-        bsurface=pygame.image.load("graphics/Sky9.png")
+        bsurface=pygame.image.load("graphics/Sky10.jpg")
         bsurface=pygame.transform.scale(bsurface,(800,350))
         screen.blit(bsurface,(0,0))
         screen.blit(player_stand,playerstand_rect)
@@ -243,9 +299,6 @@ while True:
         game_name=game_font.render("Infinite Runner",False,"Crimson")
         screen.blit(game_name,(250,70))
 
-
-
-        
     pygame.display.update()#for updating the initial display surface
     clock.tick(60)#The while true loop should not run faster than 60 times a sec
 
